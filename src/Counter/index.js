06fa7e9bffafa78afde6d6fe259bd7dbc67239f8
rqrqrqrq <rqrqrqrq@users.noreply.github.com>
2018-02-12 22:04:34 +0300

    add comments

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,3 +1,6 @@
+// Model + Container combination
+// useful for simple things like counter
+
 import React from 'react';
 import { CounterModel } from './Model';
 import { CounterContainer } from './Container';
